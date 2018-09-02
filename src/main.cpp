02f2b7c1cbb890c8c0b8cc2e05e309edf77c4ba1
Daniel S <dsimonkay@gmail.com>
2018-09-03 01:18:05 +0200

    Committing my solution.

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,33 @@
 #include <vector>
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
-#include "MPC.h"
 #include "json.hpp"
+#include "MPC.h"
 
 // for convenience
 using json = nlohmann::json;
+
+// defining the latency of the actuators (in milliseconds)
+const int ACTUATOR_LATENCY = 100;
+
+// This value assumes the model presented in the classroom is used.
+//
+// It was obtained by measuring the radius formed by running the vehicle in the
+// simulator around in a circle with a constant steering angle and velocity on a
+// flat terrain.
+//
+// Lf was tuned until the the radius formed by the simulating the model
+// presented in the classroom matched the previous radius.
+//
+// This is the length from front to CoG that has a similar radius.
+extern const double Lf = 2.67;
+
 
 // For converting back and forth between radians and degrees.
 constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
+
 
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
@@ -32,20 +49,24 @@ string hasData(string s) {
   return "";
 }
 
+
 // Evaluate a polynomial.
 double polyeval(Eigen::VectorXd coeffs, double x) {
+
   double result = 0.0;
   for (int i = 0; i < coeffs.size(); i++) {
     result += coeffs[i] * pow(x, i);
   }
+
   return result;
 }
+
 
 // Fit a polynomial.
 // Adapted from
 // https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl#L676-L716
-Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
-                        int order) {
+Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals, int order) {
+
   assert(xvals.size() == yvals.size());
   assert(order >= 1 && order <= xvals.size() - 1);
   Eigen::MatrixXd A(xvals.size(), order + 1);
@@ -65,25 +86,34 @@ Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
   return result;
 }
 
+
+// Main program module
 int main() {
+
   uWS::Hub h;
 
   // MPC is initialized here!
   MPC mpc;
 
-  h.onMessage([&mpc](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
-                     uWS::OpCode opCode) {
+  // Debug info
+  std::cout << std::endl << "Actuator latency: " << ACTUATOR_LATENCY << " milliseconds" << std::endl << std::endl;
+
+  h.onMessage([&mpc](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
     string sdata = string(data).substr(0, length);
-    cout << sdata << endl;
+    // cout << sdata << endl;
     if (sdata.size() > 2 && sdata[0] == '4' && sdata[1] == '2') {
+
       string s = hasData(sdata);
       if (s != "") {
+
         auto j = json::parse(s);
         string event = j[0].get<string>();
         if (event == "telemetry") {
+
           // j[1] is the data JSON object
           vector<double> ptsx = j[1]["ptsx"];
           vector<double> ptsy = j[1]["ptsy"];
@@ -91,57 +121,145 @@ int main() {
           double py = j[1]["y"];
           double psi = j[1]["psi"];
           double v = j[1]["speed"];
+          double delta = j[1]["steering_angle"];
+          double a = j[1]["throttle"];
 
-          /*
-          * TODO: Calculate steering angle and throttle using MPC.
-          *
-          * Both are in between [-1, 1].
-          *
-          */
-          double steer_value;
-          double throttle_value;
+          // Debug output
+          std::cout << "[x, y, psi, v, delta, a]: " << px << ", " << py <<
+                       ", " << psi << ", " << v << ", " << delta << ", " << a;
 
-          json msgJson;
+          // Common index variable
+          size_t i;
+
+          // Transforming waypoint coordinates to coordinates in the vehicle's coordinate system
+          Eigen::VectorXd ptsx_car(ptsx.size());
+          Eigen::VectorXd ptsy_car(ptsy.size());
+          double waypoint_x, waypoint_y;
+          for( i = 0;  i < ptsx.size();  i++ ) {
+
+            // The map coordinates of the ith waypoint relative to the car
+            waypoint_x = ptsx[i] - px;
+            waypoint_y = ptsy[i] - py;
+
+            // Performing the homogeneous transformation from the car's point of view (=base is (0,0))
+            ptsx_car[i] = 0.0 + cos(-psi) * waypoint_x - sin(-psi) * waypoint_y;
+            ptsy_car[i] = 0.0 + sin(-psi) * waypoint_x + cos(-psi) * waypoint_y;
+          }
+
+          // Fitting the polynomial
+          auto coeffs = polyfit(ptsx_car, ptsy_car, 3);
+
+          // Defining the "original" initial state. We're in the car's coordinate system now:
+          // the vehicle is located at (0, 0).
+          double x = 0.0;
+          double y = 0.0;
+          psi = 0.0;
+          // The initial value of 'v' is known, v = v
+
+          // polyeval(coeffs, 0) delivers simply coeffs[0], because x = 0
+          double cte = coeffs[0]; // polyeval(coeffs, 0); 
+
+          // f'(x) = coeffs[1] + 2*coeffs[2]*x + 3*coeffs[3]*x^2  ==>  for x = 0 only coeffs[1] remains
+          double epsi = -atan(coeffs[1]);
+
+          // Debug output
+          std::cout << "    [cte, epsi]: " << cte << ", " << epsi;
+
+          // The original initial state vector would look like:
+          // state << 0.0, 0.0, 0.0, v, cte, epsi;
+
+          // Considering actuator latency: let the vehicle continue driving for the specified amount of time,
+          // then update the state variables and run the solver with the new "initial" state.
+          const double latency = ACTUATOR_LATENCY / 1000.0;  // in seconds
+
+          // Determining the new initial state variables
+          x += v * latency;                     // _[t+1] = x[t] + v[t] * cos(psi[t]) * dt
+          // y remains unchanged, as there's no motion along the Y axis in the car's coordinate system.
+          // In other words: y[t+1] = y[t] + v[t] * sin(psi[t]) ==> equals to zero because sin(psi) = 0
+          psi -= v / Lf * delta * latency;      // psi_[t+1] = psi[t] - v[t] / Lf * delta[t] * dt
+          v += a * latency;                     // v_[t+1] = v[t] + a[t] * dt
+          cte += v * sin(epsi) * latency;       // cte[t+1] = f(x[t]) - y[t] + v[t] * sin(epsi[t]) * dt
+          epsi -= v * delta / Lf * latency;     // epsi[t+1] = psi[t] - psides[t] - v[t] * delta[t] / Lf * dt
+
+          // Defining the state vector, then 
+          Eigen::VectorXd state(6);
+          state << x, y, psi, v, cte, epsi;
+
+          // Solving the curent problem
+          auto vars = mpc.Solve(state, coeffs);
+
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
           // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
+          double steer_value = vars[0] / deg2rad(25);
+          double throttle_value = vars[1];
+
+          // Due to the delayed nature of the actuator execution (and due to the car having a given
+          // turning radius and an inertia) we have to take the actuator latency into consideration.
+          // We practically instruct the vehicle to do a desired angle change more slowly, in smaller
+          // steps in order to prevent oversteering and losing control over the car.
+          // 2.0 as divider seems to work well for a latency of 100ms (ref. speed: 80 mph)
+          // 4.0 as divider is still okay for a latency of 200ms (ref. speed: 80 mph)
+          if ( ACTUATOR_LATENCY > 50 ) {
+            steer_value /= 2.0 * (ACTUATOR_LATENCY / 100.0);
+          }
+
+          // Debug output
+          std::cout << "    ==>  [delta, a]: " << steer_value << ", " << throttle_value << std::endl;
+
+          json msgJson;
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = throttle_value;
 
-          //Display the MPC predicted trajectory 
+          // Display the MPC predicted trajectory 
           vector<double> mpc_x_vals;
           vector<double> mpc_y_vals;
 
-          //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
-          // the points in the simulator are connected by a Green line
+          // Extracting trajectory (x,y) values from the result returned by the solver
+          size_t const N = (vars.size() - 2) / 2;
+          for( i = 0; i < N; i++ ) {
 
+            mpc_x_vals.push_back(vars[2 + 2 * i]);
+            mpc_y_vals.push_back(vars[2 + 2 * i + 1]);
+          }
+
+          // Add (x,y) points to list here, points are in reference to the vehicle's coordinate system.
+          // The points in the simulator are connected by a green line
           msgJson["mpc_x"] = mpc_x_vals;
           msgJson["mpc_y"] = mpc_y_vals;
 
-          //Display the waypoints/reference line
+          // Display the waypoints/reference line
           vector<double> next_x_vals;
           vector<double> next_y_vals;
 
-          //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
-          // the points in the simulator are connected by a Yellow line
+          // A simple solution would be to pass on the transformed waypoint coordinates
+          // next_x_vals.resize(ptsx_car.size());
+          // next_y_vals.resize(ptsx_car.size());
+          // Eigen::VectorXd::Map(&next_x_vals[0], ptsx_car.size()) = ptsx_car;
+          // Eigen::VectorXd::Map(&next_y_vals[0], ptsy_car.size()) = ptsy_car;
 
+          // ...but a smoother line looks nicer, of course :)
+          for ( x = 0.0; x < 91.0; x += 3.0 ) {
+            next_x_vals.push_back(x);
+            next_y_vals.push_back(polyeval(coeffs, x));
+          }
+
+          // Add (x,y) points to list here, points are in reference to the vehicle's coordinate system.
+          // The points in the simulator are connected by a yellow line
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
 
 
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
+          // std::cout << msg << std::endl;
+
           // Latency
-          // The purpose is to mimic real driving conditions where
-          // the car does actuate the commands instantly.
-          //
-          // Feel free to play around with this value but should be to drive
-          // around the track with 100ms latency.
-          //
-          // NOTE: REMEMBER TO SET THIS TO 100 MILLISECONDS BEFORE
-          // SUBMITTING.
-          this_thread::sleep_for(chrono::milliseconds(100));
+          // The purpose is to mimic real driving conditions where the car does actuate the commands instantly.
+          // Feel free to play around with this value but should be to drive around the track with 100ms latency.
+          // NOTE: REMEMBER TO SET THIS TO 100 MILLISECONDS BEFORE SUBMITTING.
+          this_thread::sleep_for(chrono::milliseconds(ACTUATOR_LATENCY));
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
+
       } else {
         // Manual driving
         std::string msg = "42[\"manual\",{}]";
@@ -153,11 +271,12 @@ int main() {
   // We don't need this since we're not using HTTP but if it's removed the
   // program
   // doesn't compile :-(
-  h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data,
-                     size_t, size_t) {
+  h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t, size_t) {
+
     const std::string s = "<h1>Hello world!</h1>";
     if (req.getUrl().valueLength == 1) {
       res->end(s.data(), s.length());
+
     } else {
       // i guess this should be done more gracefully?
       res->end(nullptr, 0);
@@ -168,8 +287,7 @@ int main() {
     std::cout << "Connected!!!" << std::endl;
   });
 
-  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
-                         char *message, size_t length) {
+  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length) {
     ws.close();
     std::cout << "Disconnected" << std::endl;
   });
@@ -177,9 +295,11 @@ int main() {
   int port = 4567;
   if (h.listen(port)) {
     std::cout << "Listening to port " << port << std::endl;
+
   } else {
     std::cerr << "Failed to listen to port" << std::endl;
     return -1;
   }
+
   h.run();
 }
---
+++ writeup.md
@@ -0,0 +1,94 @@
+# Model Predictive Control Project
+
+The goal of this project was to build an MPC controller that can be used to steer and drive the the vehicle in the Term 2 simulator around the lake.
+
+The simulator delivers information over a local websocket, providing the following values to the program:
+ * global waypoint positions \[(x<sub>1</sub>, y<sub>1</sub>), (x<sub>2</sub>, y<sub>2</sub>), ... (x<sub>n</sub>, y<sub>n</sub>)\],
+ * orientation of the vehicle (in radians),
+ * global position of the vehicle (x, y),
+ * current steering angle (in radians),
+ * current throttle value (\[-1, 1\]),
+ * current speed (in MPH).
+
+The simulator expects a steering angle and a throttle value. Both values have to be normalized into the \[-1, 1\] range.
+
+## [Rubric](https://review.udacity.com/#!/rubrics/896/view) Points:
+
+
+### Compilation
+
+#### Your code should compile
+The code compiles without errors or warnings. I've applied some changes to the Eigen library source code in order to get rid of the warnings which I encountered during compilation. I made changes to the following files:
+ * [src/Eigen-3.3/Eigen/src/Core/AssignEvaluator.h](https://bitbucket.org/eigen/eigen/commits/b4f969795d1b0adbb43ebdd8c6bbcb42cb559687?at=3.3)
+ * [src/Eigen-3.3/Eigen/src/Core/products/GeneralMatrixVector.h](https://bitbucket.org/eigen/eigen/commits/131da2cbc6958c6576d23872946aac2da5a8467c?at=3.3)
+
+Source: [1402 - Warning: enum constant in boolean context](http://eigen.tuxfamily.org/bz/show_bug.cgi?id=1402#c3)
+
+
+### Implementation
+
+#### The Model
+I used the simple kinematic vehicle model that was introduced in the classroom. It uses a state vector by which we can keep track of the vehicle's current state and which enables us to make predictions about the vehicle's future states using actuated inputs.
+
+ The model consists of the following elements:
+ 
+ * the x coordinate of the vehicle (*x*),
+ * the y coordinate of the vehicle (*y*),
+ * the orientation of the vehicle (&psi;),
+ * the velocity of the vehicle (*v*)
+ * the current cross track error (*cte*),
+ * the current orientation error (*e&psi;*),
+ * the control input value for the steering angle (&delta;),
+ * the control input value for acceleration/deceleration (*a*).
+
+The state vector consists of the first six elements of the model components listed above: \[ x, y, &psi;, v, cte, e&psi; \]
+
+If the state vector and the control inputs are known, we can calculate the values of the new state variables in *dt* time using the following equations:
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;x<sub>t+1</sub> = x<sub>t</sub> + v<sub>t</sub> \* cos(&psi;<sub>t</sub>)  \* dt
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;y<sub>t+1</sub> = y<sub>t</sub> + v<sub>t</sub> \* sin(&psi;<sub>t</sub>)  \* dt
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&psi;<sub>t+1</sub> = &psi;<sub>t</sub> + v<sub>t</sub> / Lf \*   &delta;<sub>t</sub> \* dt
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;v<sub>t+1</sub> = v<sub>t</sub> + a<sub>t</sub> \* dt
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;cte<sub>t+1</sub> = cte<sub>t</sub> + v<sub>t</sub> \* sin(e&psi;<sub>t</sub>) \* dt
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;e&psi;<sub>t+t</sub> = e&psi;<sub>t</sub> + v<sub>t</sub> / Lf  \* &delta;<sub>t</sub> \* dt
+
+cte<sub>t</sub> is the difference between the itinary and the current vehicle position. If we have a polynomial *f* which describes the refrence line, then the cross track error at time *t* is: cte<sub>t</sub> = f(x<sub>t</sub>) - y<sub>t</sub>
+
+e&psi;<sub>t</sub> is the difference between the current and the desired orientation of the vehicle: e&psi;<sub>t</sub> = &psi;<sub>t</sub> - &psi;des<sub>t</sub>
+&psi;des<sub>t</sub> can be calculated as the tangential angle of the above mentioned polynomial *f* at *x<sub>t</sub>*, or arctan(f'(x<sub>t</sub>)), so e&psi;<sub>t</sub> = &psi;<sub>t</sub> - arctan(f'(x<sub>t</sub>))
+
+
+#### Timestep Length and Elapsed Duration (N & dt)
+At first I was brave enough to set the prediction horizon to 2.0 seconds and tried these *(N, dt)* values:
+ * 40 &ndash; 0.05 &mdash; did not work: the vecicle drove off-track before the first curve
+ * 20 &ndash; 0.1 &mdash; worked, but was not stable enough
+ * 10 &ndash; 0.2 &mdash; didn't work
+
+Then I set the prediction horizon to 1.5 seconds and tried these *(N, dt)* values:
+ * 20 &ndash; 0.075 &mdash; yielded un unstable behaviour
+ * 10 &ndash; 0.15 &mdash; it did work, although the vehicle slowed down considerably in the curves
+ * 5 &ndash; 0.3 &mdash; the car left the track right after start
+ 
+ After that I kept on decreasing the prediction horizon until I got a stable behaviour around 1 second. A few tries:
+ * 12 &ndash; 0.08 &mdash; erratic trajectory lines in the second and third curves (although driving in the curves faster than in the "stable" solution)
+ * 11 &ndash; 0.09 &mdash; erratic trajectory lines in the second and third curves (and also slowing down in the curves)
+ * 9 &ndash; 0.1 &mdash; stable solution
+ * 10 &ndash; 0.1 &mdash; stable solution
+ * 11 &ndash; 0.1 &mdash; stable solution
+ * 12 &ndash; 0.1 &mdash; pretty stable solution
+ 
+ At the end picked a stable solution, choosing 10 for N and 0.1 for dt.
+
+
+#### Polynomial Fitting and MPC Preprocessing
+Before fitting a polynomial to the waypoint coordinates, I transformed these into the vehicle's coordinate system using the homogeneous transformation. Using the vehicle's coordinate system sets *x*, *y* and *&psi;* to zero in the state vector (as the car is at the origo of its coordinate system, heading towards the **x** axis). It also greatly simplifies the further calculation of *cte* and *e&psi;*.
+
+
+#### Model Predictive Control with Latency
+The idea behind incorporating actuator latency into the system is simple: after having calculated the initial (=current) state of the car, we virtually let the vehicle keep on driving for the predefined amount of time (=latency) and update the state vector by the changes made to its components during the elapsed time. We then run the MPC solver with the updated state vector as the new initial state. As a result we receive exactly the control input values that have to be executed after the predefined time delay in order to keep the vehicle on the desired track.
+
+
+### Simulation
+
+#### The vehicle must successfully drive a lap around the track
+The car can successfully drive around the lake with a reference speed of 80 MPH.
